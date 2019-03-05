38a82da464f74cc1884ff4fa1204718cf0531fd9
khaled764 <kaalosaimi@gmail.com>
2019-03-05 18:43:02 +0300

    Week 6 HW

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,29 @@
 // click event for gray button
-
+$("#grayButton").on("click",switchGray);
 // click event for white button
-
+$("#whiteButton").on("click",switchWhite);
+// function to change color to gray
 function switchGray() {
     // change background to gray
-    // change font to white
+     // change font to white
+      let body = $("body");
+      body.css({
+        "background":"gray",
+        "color":"white"
+      });
 }
-
-function switchWhite() {
-    // change background to white
-    // change font to black
+// function to change color to white
+ function switchWhite() {
+     // change background to white
+     // change font to black
+     let body = $("body");
+     body.css({
+       "background":"white",
+       "color":"black"
+     });
 }
+//function switchWhite() {
+//let body = $("body");
+//body.css("background,white");
+//body.css("background,black");
+//}
